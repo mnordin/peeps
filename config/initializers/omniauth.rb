988490898c13873be67d8acce026766b1cf9fb66
Markus Nordin <markus.nordin@gmail.com>
2011-07-07 13:17:08 +0200

    Trying some more scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.configuration.middleware.use OmniAuth::Builder do
   provider :google_apps, nil, {:callback_path => "/auth/google_apps/callback", :domain => 'newsdesk.se'}
-  provider :facebook, "194756060575091", "285cebd99fba497fe071ecb221733775"
+  provider :facebook, "194756060575091", "285cebd99fba497fe071ecb221733775", {:scope => ["user_location", "user_online_presence", "user_photos", "user_status", "read_stream", "manage_pages"]}
 end
