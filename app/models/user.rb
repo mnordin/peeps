ca34bd2bb37a5971524d7c52f92fee0a23e1cff8
Markus Nordin <markus.nordin@gmail.com>
2011-07-07 09:36:14 +0200

    Removed markup for user uploaded images

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,5 @@
 class User < ActiveRecord::Base
   validates :email, :presence => true, :uniqueness => true
-  
-  # Paperclip
-  # Soon to be deprecated
-  has_attached_file :photo,
-    :path => "/:rails_root/public/:class/:attachment/:id/:style_:basename.:extension",
-    :url => "/:class/:attachment/:id/:style_:basename.:extension",
-    :styles => {
-      :thumb => "250x250>",
-      :slideshow => "550x550>"
-    },
-    :default_style => :small
 
   def name
     "#{first_name} #{last_name}"
