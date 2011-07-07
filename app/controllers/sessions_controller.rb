9a7133edf0f5d732488a0addbd3f792418e58a09
Markus Nordin <markus.nordin@gmail.com>
2011-07-07 11:15:41 +0200

    Omniauth response was wrongly named

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def facebook_callback
-    render :json => request.env["omniauth.env"].to_yaml
+    render :json => request.env["omniauth.auth"].to_yaml
   end
 
   def failure
