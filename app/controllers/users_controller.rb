139bf5ddcc944acaebe6680ba283bef9c083efcc
Markus Nordin <markus.nordin@gmail.com>
2011-07-09 15:33:19 +0200

    removed the next method, future feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,8 +36,7 @@ class UsersController < ApplicationController
     if session[:fb_access_token]
       user = FbGraph::User.me(session[:fb_access_token])
       user.fetch
-      @photos = user.photos, user.photos.next
-      @photos.map! { |photo| photo.images }
+      @photos = user.photos.map { |photo| photo.images }
       logger.info @photos
     end
   end
