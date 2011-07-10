eb9f60db443ddedb387532a464957629a13cedeb
Markus Nordin <markus.nordin@gmail.com>
2011-07-10 02:21:44 +0200

    Kind of working player added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,49 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var currentFirstName;
+var currentPeepImage;
+var peeps = [];
+
+function play(){
+    $("html").addClass("playing");
+    $("#guess").focus();
+
+    shuffle();
+    changePeep(); // sets up the first peep as well as changes it later on
+};
+
+function shuffle(){
+    $("ul#peeps li img").each(function(){
+       peeps.push([$(this).data("first-name"), $(this).attr("src")]);
+    });
+    // randomize the array
+    //return peeps.sort(Math.round(Math.random())-0.5);    FF5 SAYS THIS IS INVALID
+};
+
+function changePeep(){
+    currentFirstName = peeps[0][0];
+    currentPeepImage = peeps[0][1];
+    $("#currentPeepImage").attr("src", peeps[0][1]);
+}
+
+function guess(){
+    guessed_name = $("#guess").val();
+    if(guessed_name.match(currentFirstName)){
+        success();
+    };
+}
+
+function success(){
+    //removes the first element in the array
+    peeps.splice(0,1);
+    //console.log("SUCCESS! " + guessed_name + " WAS CORRECT!");
+    $("#guess").val("");
+    changePeep();
+}
+
+$(document).ready(function(){
+    $("#guess-form").submit(function(){
+        guess();
+        return false;
+    });
+    $("#play").click(play());
+    play();
+});
