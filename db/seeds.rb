c98f537ed9491bc723fac6282ed0c64e0a1ff97b
Markus Nordin <markus.nordin@gmail.com>
2011-09-08 15:33:13 +0200

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+Locale.create(:name => "Sweden", :code => "se")
+Locale.create(:name => "Norway", :code => "no")
+Locale.create(:name => "Denmark", :code => "dk")
+Locale.create(:name => "Finland", :code => "fi")
+Locale.create(:name => "Singapore", :code => "sg")
+Locale.create(:name => "England", :code => "uk")
+
+Office.create(:name => "Stockholm", :locale => Locale.find_by_name("Sweden"), :code => "st")
+Office.create(:name => "Gothenburg", :locale => Locale.find_by_name("Sweden"), :code => "gt")
+Office.create(:name => "Malmoe", :locale => Locale.find_by_name("Sweden"), :code => "ma")
+Office.create(:name => "Oslo", :locale => Locale.find_by_name("Norway"), :code => "os")
+Office.create(:name => "Copenhagen", :locale => Locale.find_by_name("Denmark"), :code => "co")
+Office.create(:name => "Helsinkki", :locale => Locale.find_by_name("Sweden"), :code => "he")
+Office.create(:name => "Singapore", :locale => Locale.find_by_name("Singapore"), :code => "sg")
+Office.create(:name => "London", :locale => Locale.find_by_name("England"), :code => "ln")
