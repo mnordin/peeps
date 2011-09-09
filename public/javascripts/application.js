445c9dd55a15d18512bf7abeb851545154ba6b61
Markus Nordin <markus.nordin@gmail.com>
2011-09-09 09:32:28 +0200

    Removes utf8 field from change office form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    
+
+    // removes utf-8 input
+    $("form#change-office input[type=hidden]").remove();
+
     $("select#office").change(function(){
         $(this).parents("form").submit();
     });
